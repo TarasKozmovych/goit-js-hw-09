69880fcee618bc799e0aed74d9ade2891d138f4e
Taras Kozmovych <taraskom0404@gmail.com>
2022-08-14 18:48:18 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    bodyColor: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start'),
+    stopBtn: document.querySelector('button[data-stop'),
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn(e) {
+    intervalId = setInterval(() => {
+        refs.bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startBtn.disabled = true;
+};
+function onStopBtn() {
+    clearInterval(intervalId);
+    refs.startBtn.disabled = false;
+}
 
